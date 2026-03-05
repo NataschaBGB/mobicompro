9073dbcf5160e144b517a09b97803293390286bc
NataschaBGB <nannabjerning@hotmail.com>
2026-03-05 11:50:05 +0100

    add: componenets for home page

---
+++ src/components/SmartSettings/SmartSettings.jsx
@@ -0,0 +1,27 @@
+import './SmartSettings.sass';
+
+
+export default function SmartSettings() {
+
+    return (
+        <section className="smart-settings">
+            <h2>Smart Indstillinger</h2>
+            {/* light component with smart settings - */}
+            {/* title (lys i køkken)    | title (lys i indkørsel) */}
+            {/* label + on/off button */}
+            {/* title (varme i køkken)  | title (lys i garage) */}
+            {/* label + on/off button */}
+
+            {/* Heat Component gets some data from API (more data is shown on heat page)*/}
+            {/* heat component with smart settings - */}
+            {/* title (varme i køkken)  | title (varme i soveværelse) */}
+            {/* label + on/off button */}
+            {/* thermometer icon |  temperature display
+                                    current mode */}
+            {/* Settings button with thermometer icon  */}
+            {/* import { LiaThermometerHalfSolid } from "react-icons/lia"; */}
+            {/* <LiaThermometerHalfSolid /> */}
+        </section>
+    )
+
+}
