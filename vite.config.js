57ec77b810e373b375b9e1968939a29565d00ba6
NataschaBGB <nannabjerning@hotmail.com>
2026-03-11 10:09:27 +0100

    fix: basepath

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/mobicompro/",
+  // base: "/mobicompro/",
   plugins: [react()],
   build: {
     outDir: 'docs',
