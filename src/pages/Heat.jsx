8209257cf954b62724628f7ce7b8daf21cfc81e8
NataschaBGB <nannabjerning@hotmail.com>
2026-03-16 09:52:56 +0100

    add: outside weather temp in thermostat
    added api loader for the weather to add outside temp on thermostat instead of hardcoded temp

--- src/pages/Heat.jsx
+++ src/pages/Heat.jsx
@@ -8,8 +8,9 @@ import HeatMode from "../components/HeatMode/HeatMode";
 
 export default function Heat() {
 
-    const devices = useLoaderData();
+    const { devices, weather } = useLoaderData();
     console.log("devices", devices);
+    console.log("weather", weather);
 
     
     return (
@@ -18,23 +19,26 @@ export default function Heat() {
             <Header showBurgerMenu={false} showBackButton={true} showOptions={true} title="Varme" />
     
             <main className="heat">
+                {/* loop through devices */}
                 {devices && devices.length > 0 ? (
                     devices.map((device) => (
-                    <div key={device.id}>
+                        // key = unique id for each device, so React can keep track of them when they change
+                        // show data for each device in the API - name, current temp, target temp, mode and weather with that id
+                        <div key={device.id}>
 
-                        <HeatThermostat device={device} />
+                            <HeatThermostat device={device} weather={weather} />
 
-                        <HeatVent device={device} />
+                            <HeatVent device={device} />
 
-                        <HeatMode device={device} />
+                            <HeatMode device={device} />
 
-                        <button className="heat_on-off-button">
-                            {/* on click - set state and save in useLocalStorage hook */}
-                            on/off icon
-                        </button>
+                            <button className="heat_on-off-button">
+                                {/* on click - set state and save in useLocalStorage hook */}
+                                on/off icon
+                            </button>
 
-                    </div>
-                ))
+                        </div>
+                    ))
                 ) : (
                     <section className="heat__no-devices">
                     <h3>Ingen data blev fundet for dette device</h3>
