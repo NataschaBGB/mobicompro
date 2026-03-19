0b29df30b2819ef747146ccecbe7a504204056da
NataschaBGB <nannabjerning@hotmail.com>
2026-03-19 10:25:32 +0100

    fix: moved hooks folder and readme
    I moved the hooks folder and changed all paths that use hooks in necessary components.
    I added documentation to the readme

--- src/components/Preset/Preset.jsx
+++ src/components/Preset/Preset.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import useLocalStorage from "../hooks/useLocalStorage";
+import useLocalStorage from "../../hooks/useLocalStorage";
 import PresetSetting from "./PresetSetting";
 import { PresetData } from "./PresetData";
 import './Preset.sass'
